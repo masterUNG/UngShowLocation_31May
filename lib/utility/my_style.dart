76cd7ca389fcd45e0e2a84f5d30a622cbd5013cd
masterUNG <phrombutr@gmail.com>
2021-05-31 13:02:50 +0700

    Test

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -5,6 +5,8 @@ class MyStyle {
   Color primary = Color(0xff8f1cc4);
   Color light = Color(0xffc455f8);
 
+  // Test
+
   TextStyle h1Style() => TextStyle(
         fontSize: 24,
         fontWeight: FontWeight.bold,
